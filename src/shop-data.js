1f6aed3801d72f696871d678b5d6b8e3679ebde4
Habid B Espinosa <hab.bad@hotmail.com>
2022-07-28 15:13:14 +0200

    try with env vars 2

--- src/shop-data.js
+++ src/shop-data.js
@@ -10,7 +10,7 @@ const SHOP_DATA = [
       },
       {
         id: 2,
-        name: "Blue Beanie",
+        name: "Blue Beanies",
         imageUrl: "https://i.ibb.co/ypkgK0X/blue-beanie.png",
         price: 18,
       },
