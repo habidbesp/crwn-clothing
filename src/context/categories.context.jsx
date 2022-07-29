44ff16d2c68ffb816d8e7ca6e2146e3224644873
Habid B Espinosa <hab.bad@hotmail.com>
2022-07-29 13:08:01 +0200

    migrated to useReducer and helpers recudcers seted

--- src/context/categories.context.jsx
+++ src/context/categories.context.jsx
@@ -1,13 +1,45 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useReducer, useEffect } from "react";
 // import SHOP_DATA from "../shop-data.js";
 import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
+
+import { createAction } from "../utils/reducer/reducer.utils";
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
 });
 
+export const CATEGORIES_ACTION_TYPES = {
+  SET_CATEGORIES_MAP: "SET_CATEGORIES_MAP",
+};
+
+const categoriesReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case CATEGORIES_ACTION_TYPES.SET_CATEGORIES_MAP:
+      return {
+        ...state,
+        categoriesMap: payload,
+      };
+    default:
+      throw new Error(`Unhandled type ${type} in categoriesReducer`);
+  }
+};
+
+const INITIAL_STATE = { categoriesMap: {} };
+
 export const CategoriesProvider = ({ children }) => {
-  const [categoriesMap, setCategoriesMap] = useState({});
+  // const [categoriesMap, setCategoriesMap] = useState({});
+  const [{ categoriesMap }, dispatch] = useReducer(
+    categoriesReducer,
+    INITIAL_STATE
+  );
+
+  const setCategoriesMap = (categorieMap) => {
+    dispatch(
+      createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES_MAP, categorieMap)
+    );
+  };
 
   useEffect(() => {
     // addCollectionAndDocuments("categories", SHOP_DATA);
